5f98cfbd05c14f4bb2815c3222f4ebe47995002f
fiyosa <fiyosa97@gmail.com>
2023-09-16 18:24:40 +0700

    edit

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pages/App'
+import Path from './pages/Path'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Path />
   </React.StrictMode>
 )
